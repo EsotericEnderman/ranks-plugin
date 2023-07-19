e29f449dfefc135bbe03c5b1dce47bdc926cbef6
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-19 21:36:32 +0100

    💬 Added highest priority to chat event. (Other plugins can cancel)

--- src/main/java/net/slqmy/rank_system/events/listeners/AsyncPlayerChatEventListener.java
+++ src/main/java/net/slqmy/rank_system/events/listeners/AsyncPlayerChatEventListener.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.jetbrains.annotations.NotNull;
@@ -12,29 +13,33 @@ import net.slqmy.rank_system.RankSystem;
 import net.slqmy.rank_system.managers.RankManager;
 
 public final class AsyncPlayerChatEventListener implements Listener {
-  private final RankManager rankManager;
+	private final RankManager rankManager;
 
-  public AsyncPlayerChatEventListener(@NotNull final RankSystem plugin) {
-    this.rankManager = plugin.getRankManager();
-  }
+	public AsyncPlayerChatEventListener(@NotNull final RankSystem plugin) {
+		this.rankManager = plugin.getRankManager();
+	}
 
-  @EventHandler
-  public void onAsyncPlayerChat(final @NotNull AsyncPlayerChatEvent event) {
-    // Custom chat message:
-    // (rank formatting) <rank name> (white) <player> » (grey) <message>
-    event.setCancelled(true);
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onAsyncPlayerChat(final @NotNull AsyncPlayerChatEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
 
-    final Player player = event.getPlayer();
+		// Custom chat message:
+		// (rank formatting) <rank name> (white) <player> » (grey) <message>
+		event.setCancelled(true);
 
-    final String rankDisplayName = rankManager.getPlayerRank(player.getUniqueId(), false).getDisplayName();
+		final Player player = event.getPlayer();
 
-    // Note: colour codes that represent colours actually reset the previous colour
-    // codes.
+		final String rankDisplayName = rankManager.getPlayerRank(player.getUniqueId(), false).getDisplayName();
 
-    // (If the display name is blank)
-    Bukkit.broadcastMessage(
-        (rankDisplayName.equals(ChatColor.RESET.toString() + ChatColor.RESET) ? "" : rankDisplayName + " ")
-            + player.getName()
-            + " » " + ChatColor.GRAY + event.getMessage());
-  }
+		// Note: colour codes that represent colours actually reset the previous colour
+		// codes.
+
+		// (If the display name is blank)
+		Bukkit.broadcastMessage(
+				(rankDisplayName.equals(ChatColor.RESET.toString() + ChatColor.RESET) ? "" : rankDisplayName + " ")
+						+ player.getName()
+						+ " » " + ChatColor.GRAY + event.getMessage());
+	}
 }
